02511c07605b690d5de73ff57d841160d8ff7db9
Fatemeh Alinejad <fatemeh.alinejad.dev@gmail.com>
2024-12-18 23:31:57 +0100

    tailwind was intalled and its configuration has been set

---
+++ client/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        text: "#000000",
+        background: "#FEFDFB",
+        primary: "#8A817B",
+        secondary: "#E0AE9F",
+        accent: "#722231",
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
