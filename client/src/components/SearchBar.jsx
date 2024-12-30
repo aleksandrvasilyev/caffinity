58f0a1a907216e5b06b9e7315b4f6c16dfb73b5a
Alia Taher <alia.taher022@gmail.com>
2024-12-30 17:06:38 +0100

    feat: create SearchBar component
    - Created SearchBar component with filter and search icon subcomponents.
    - Implemented FilterDropdown functionality.
    - Added tests for the SearchBar component.
    - Updated index.css with custom scrollbar styles.
    - Added "Caffinity" to workspace.

---
+++ client/src/components/SearchBar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import SearchIcon from "./SearchIcon";
+import Filters from "./Filters";
+import useFetch from "../hooks/useFetch";
+
+const SearchBar = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedFilters, setSelectedFilters] = useState([]);
+  
+  // searchResults is not used in this component it will be used in catalog page to display the search results.
+  // eslint-disable-next-line no-unused-vars
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearchResults = (data) => {
+    setSearchResults(data);
+  };
+
+  const { isLoading, error, performFetch } = useFetch(
+    "/search",
+    handleSearchResults,
+  );
+
+  const handleSearch = () => {
+    performFetch({
+      method: "POST",
+      body: JSON.stringify({
+        query: searchQuery,
+        filters: selectedFilters,
+      }),
+    });
+  };
+
+  return (
+    <>
+      <div className=" relative bg-white px-6 py-4 mx-10 my-10 border-2 rounded-full flex sm:justify-center sm:items-center flex-row h-50 border-black sm:flex-row justify-end items-end">
+        {isLoading && <div> Loading...</div>}
+        {error && <div className=" px-auto py-6 text-lg">Error: {error}</div>}
+
+        <input
+          className="h-[30%] px-3 pb-2 rounded-full w-[100%] focus:outline-none hover:none pl-10 text-text text-ellipsis bg-transparent "
+          type="text"
+          placeholder="Search for cafes by name, city, or features"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button onClick={handleSearch} aria-label="search">
+          <span>
+            <SearchIcon aria-hidden="true" />
+          </span>
+        </button>
+        <div>
+          <Filters onFilterChange={setSelectedFilters} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SearchBar;
