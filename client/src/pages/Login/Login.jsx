dfdc642d2803feeb9dc929cf35f4904c4c348b34
Fatemeh Alinejad <fatemeh.alinejad.dev@gmail.com>
2024-12-31 19:53:52 +0100

    header fixes

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Login = () => {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold">Favorites Page</h1>
+      <h1 className="text-2xl font-bold">Login Page</h1>
       <p>This is the login page.</p>
     </div>
   );
--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const SignUp = () => {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold">Favorites Page</h1>
+      <h1 className="text-2xl font-bold">Sign up Page</h1>
       <p>This is the sign up page.</p>
     </div>
   );
