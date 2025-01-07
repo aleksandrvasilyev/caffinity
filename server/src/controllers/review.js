d53f08aa0448fb405e604f482088bdd3ad8db48a
Alexander Vasyliev <saddon11@gmail.com>
2025-01-07 18:05:04 +0100

    done:delete a review

--- server/src/controllers/review.js
+++ server/src/controllers/review.js
@@ -1,4 +1,5 @@
 import { addReview, editReview } from "../util/reviews.js";
+import Review from "../models/Review.js";
 
 export const storeReview = async (req, res, next) => {
   try {
@@ -15,6 +16,20 @@ export const updateReview = async (req, res, next) => {
     const updatedReview = await editReview(req.body);
 
     res.status(200).send({ success: true, result: updatedReview });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteReview = async (req, res, next) => {
+  const reviewId = req.body.reviewId;
+
+  try {
+    await Review.deleteOne({ _id: reviewId });
+
+    res
+      .status(200)
+      .send({ success: true, result: "Review successfully deleted!" });
   } catch (error) {
     next(error);
   }
