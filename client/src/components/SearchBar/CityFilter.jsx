7629f7a2e81eca0379d378a97cd72c1130382e99
Alia Taher <alia.taher022@gmail.com>
2025-01-15 11:24:09 +0100

    feat: add CafeByCity page and implement CityFilter functionality

--- client/src/components/SearchBar/CityFilter.jsx
+++ client/src/components/SearchBar/CityFilter.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button/Button";
+import useFetch from "../../hooks/useFetch";
+import { useNavigate } from "react-router-dom";
 
 const CityFilter = () => {
+  const navigate = useNavigate();
+  const [cafeByCity, setCafeByCity] = useState([]);
+
+  const { performFetch } = useFetch("/search", setCafeByCity);
+
+  const handleClick = (e) => {
+    const city = e.target.value;
+
+    performFetch({
+      method: "GET",
+      search: city,
+    });
+
+    navigate("/cafeByCity", { state: { cafes: cafeByCity } });
+  };
+
   return (
     <div className="flex sm:flex-row flex-col flex-wrap gap-3 justify-between mx-auto my-[4%]  p-5 my-20 w-[85%] h-auto">
-      <Button className="bg-primary rounded-full text-white font-medium">
+      <Button
+        className="bg-primary rounded-full text-white font-medium"
+        value="Amsterdam"
+        onClick={handleClick}
+      >
         Amsterdam
       </Button>
 
