205c79d6bd4cf2c558dfbaf33e2205c23d14ab7c
Fatemeh Alinejad <fatemeh.alinejad.dev@gmail.com>
2025-01-16 11:09:12 +0100

    hotfix

--- client/src/components/SearchBar/CityFilter.jsx
+++ client/src/components/SearchBar/CityFilter.jsx
@@ -21,7 +21,7 @@ const CityFilter = () => {
   };
 
   return (
-    <div className="flex sm:flex-row flex-col flex-wrap gap-3 justify-between mx-auto my-[4%]  p-5 my-20 w-[85%] h-auto">
+    <div className="flex sm:flex-row flex-col flex-wrap gap-3 justify-between mx-auto p-5 my-20 w-[85%] h-auto">
       <Button
         className="bg-primary rounded-full text-white font-medium"
         value="Amsterdam"
