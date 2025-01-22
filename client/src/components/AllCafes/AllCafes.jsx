12b5cf200e3359e042396a141aa07dd9bd4d16a2
Alia Taher <alia.taher022@gmail.com>
2025-01-22 13:31:22 +0100

    Add filter by food option functionality  in AllCafes and CafeByCity components

--- client/src/components/AllCafes/AllCafes.jsx
+++ client/src/components/AllCafes/AllCafes.jsx
@@ -3,20 +3,59 @@ import { useState, useEffect } from "react";
 import useFetch from "../../hooks/useFetch/useFetch";
 import CafeCard from "../CafeCard/CafeCard";
 import Pagination from "../Pagination/Pagination";
+import FilterButtons from "../FilterButtons/FilterButtons";
 
 const AllCafes = () => {
   const [cafes, setCafes] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const { isLoading, error, performFetch } = useFetch("/cafes", setCafes);
+  const [selectedFilter, setSelectedFilter] = useState(null);
+
+  const fetchCafes = (page, filter) => {
+    const queryParams = {
+      method: "GET",
+      params: {
+        page: page,
+        limit: 10,
+      },
+    };
+
+    if (filter !== null) {
+      queryParams["food-options"] = filter;
+    }
+
+    performFetch(queryParams);
+  };
 
   useEffect(() => {
-    performFetch({ method: "GET", params: { page: currentPage, limit: 10 } });
-  }, [currentPage]);
+    fetchCafes(currentPage, selectedFilter);
+  }, [currentPage, selectedFilter]);
 
-  const results = Array.isArray(cafes?.result?.data) ? cafes.result.data : [];
+  const results = Array.isArray(cafes?.result?.data)
+    ? cafes.result.data
+    : Array.isArray(cafes?.data)
+      ? cafes.data
+      : [];
+
+  const totalPages = cafes?.result?.totalPages || cafes?.totalPages || 1;
+
+  const onFilterSelection = (filter) => {
+    setSelectedFilter(filter.index);
+    setCurrentPage(1);
+  };
+
+  const handlePageChange = (newPage) => {
+    if (newPage !== currentPage) setCurrentPage(newPage);
+  };
 
   return (
     <div>
+      <div className="mx-auto my-10 max-w-screen-md">
+        <p className="text-xl font-semibold text-center my-2">
+          Looking for a particular food option ?
+        </p>
+        <FilterButtons onFilterSelection={onFilterSelection} />
+      </div>
       {isLoading && <div className="text-center">Loading...</div>}
       {error && (
         <div className="text-center text-red-500">
@@ -33,9 +72,9 @@ const AllCafes = () => {
         )}
       </div>
       <Pagination
-        totalPages={cafes?.result?.totalPages}
+        totalPages={totalPages}
         currentPage={currentPage}
-        onPageChange={setCurrentPage}
+        onPageChange={handlePageChange}
       />
     </div>
   );
